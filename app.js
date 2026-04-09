63162f2b47fc4bac0092506e32829862b3df11ee
pro <pro@pros-MacBook-Pro.local>
2026-04-09 15:38:59 +0300

    created counter program using javascript

--- app.js
+++ app.js
@@ -51,7 +51,7 @@
 // document.getElementById("p3").textContent = `Your favourite food is ${favouriteFood}`;
 // document.getElementById("p4").textContent = `Enrolled: ${isStudent}`;
 
-//Arithmetic Operators = operands (values, variables, etc)
+// Arithmetic Operators = operands (values, variables, etc)
 //                       operators (+ - * /)
 //                     ex. 11 = x + 5;
 
@@ -83,6 +83,93 @@
 
 //  let result = 1 + 2 * 3 + 4 ** 2;
 // let result = 6 / 2 ** (2 + 5);
-let result = 6 / 2 ** 7;
+// let result = 6 / 2 ** 7;
 
- console.log(result);
+//  console.log(result);
+
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// PROFESSIONAL WAY = HTML textbox
+
+// let username;
+
+// username = window.prompt("what's your username?");
+// console.log(username);
+
+// function askName() {
+//     username = window.prompt("what's your username?")
+
+//     console.log(username);
+// }
+
+// let username;
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent = `Hello ${username}`
+
+//     console.log(username);
+// }
+
+//type conversion = change the datatype of a value to another
+//                    (strings, numbers, booleans)
+
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1
+// console.log(age);
+
+// let x = "burger";
+// let y = "burger";
+// let z = "burger";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z)
+
+// const = a variable that can't be changed
+
+// const pi = 3.14159
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter radius of the circle");
+// radius = Number(radius);
+
+// circumference = 2 * pi * radius;
+// console.log(circumference);
+
+// giving functionality to our counter
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const counterlabel= document.getElementById("counterLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    counterlabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    counterlabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    counterlabel.textContent = count;
+}
+
+
+
+
+
+
+
+
+
