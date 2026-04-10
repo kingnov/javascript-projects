8ffff9b60f530a4f0659e342171527e84dbeffae
pro <pro@pros-MacBook-Pro.local>
2026-04-10 11:01:58 +0300

    working on roll program

--- app.js
+++ app.js
@@ -171,9 +171,9 @@
 // console.log(Math.PI);
 // console.log(Math.E);
 
-let x = 3;
-let y = 2;
-let z = 1;
+// let x = 3;
+// let y = 2;
+// let z = 1;
 
 // z = Math.round(x)
 // z = Math.floor(x);
@@ -192,8 +192,29 @@ let z = 1;
 // let max = Math.max(x, y, z);
 // console.log(max);
 
-let min = Math.min(x, y, z);
-console.log(min);
+// let min = Math.min(x, y, z);
+// console.log(min);
+
+// Random Number Generator
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+// console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random * max) + min;
+    myLabel.textContent = randomNum;
+
+    console.log(randomNum);
+} 
+
 
 
 
